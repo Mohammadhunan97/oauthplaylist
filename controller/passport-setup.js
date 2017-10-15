65c9f5a9fb89e555894241b78232867438e13139
mohammad <you@example.com>
2017-10-15 17:52:54 -0400

    oauth passport basic setup

--- controller/passport-setup.js
+++ controller/passport-setup.js
@@ -1,3 +1,8 @@
 const passport = require('passport'),
-const GoogleStrategy = require('passport-google-oauth20');
+GoogleStrategy = require('passport-google-oauth20');
 
+passport.use(new GoogleStrategy({
+	// options for google strategy
+},()=>{
+	// passport callback function
+}))
